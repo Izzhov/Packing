5790a257c2b1be6da3d25e4dca10c838c015753a
Kyle VanderWerf <kylevanderwerf@yahoo.com>
2015-06-04 12:48:58 -0400

    Added shell script for my first cluster test

---
+++ Sphere2DSteepest/jobs/test_spheres_2D_10-6_6-4-15.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+dirname=testsphere2D_10-6_6-4-15
+
+mkdir ~/Packing/Sphere2DSteepest/Cluster/$dirname
+
+for i in $(seq 100); do
+    echo cd ~/Packing/Sphere2DSteepest/Cluster/$dirname '&&' ../Sphere2DSteepest --seed $((1433355833+i)) --filename $i.txt
+done
