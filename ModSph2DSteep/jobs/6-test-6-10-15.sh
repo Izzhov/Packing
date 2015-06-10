b44149b1b64d6dc709248ea64f5daa07f8220818
Kyle VanderWerf <kylevanderwerf@yahoo.com>
2015-06-10 13:10:42 -0400

    fixed directory in shell file

--- ModSph2DSteep/jobs/6-test-6-10-15.sh
+++ ModSph2DSteep/jobs/6-test-6-10-15.sh
@@ -2,7 +2,7 @@
 
 dirname=6-2D-Packings-6-10-15
 
-mkdir ~/Packing/Sphere2DSteepest/Cluster/$dirname
+mkdir ~/Packing/ModSph2DSteep/Cluster/$dirname
 
 for i in $(seq 1000); do
     echo cd ~/Packing/ModSph2DSteep/Cluster/$dirname '&&' ../Sphere2DSteepest.out --seed $((1433949739+i)) --filename $i.txt
