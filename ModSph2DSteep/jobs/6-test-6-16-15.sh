9a03e89452da52e2f6374c6f08edda1e7ba9906c
Kyle VanderWerf <kylevanderwerf@yahoo.com>
2015-06-16 11:35:14 -0400

    shell script for 6-16-15 trials

---
+++ ModSph2DSteep/jobs/6-test-6-16-15.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+dirname=6-2D-Packings-6-10-15
+
+mkdir ~/Packing/ModSph2DSteep/Cluster/$dirname
+
+for i in $(seq 100000); do
+    echo cd ~/Packing/ModSph2DSteep/Cluster/$dirname '&&' ../Sphere2DSteepest.out --seed $((1433949739+i)) --filename $i.txt
+done
