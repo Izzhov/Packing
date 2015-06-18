cfbf7ab8f60aec9fa16afd90de971517bd895ed2
Kyle VanderWerf <kylevanderwerf@yahoo.com>
2015-06-18 09:30:23 -0400

    fixed file name to execute in shell script

--- ModSph2DSteep/jobs/6-test-6-16-15.sh
+++ ModSph2DSteep/jobs/6-test-6-16-15.sh
@@ -5,5 +5,5 @@ dirname=6-2D-Packings-6-16-15
 mkdir ~/Packing/ModSph2DSteep/Cluster/$dirname
 
 for i in $(seq 100000); do
-    echo cd ~/Packing/ModSph2DSteep/Cluster/$dirname '&&' ../Sphere2DSteepest.out --seed $((1433949739+i)) --filename $i.txt
+    echo cd ~/Packing/ModSph2DSteep/Cluster/$dirname '&&' ../ModSph2DSteep.out --seed $((1433949739+i)) --filename $i.txt
 done
