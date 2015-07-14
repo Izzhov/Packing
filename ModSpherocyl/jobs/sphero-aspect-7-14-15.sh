1040b6227b4354327791089d5ffe3102a25c69cd
Kyle VanderWerf <kylevanderwerf@yahoo.com>
2015-07-14 17:32:07 -0400

    another shell script fix

--- ModSpherocyl/jobs/sphero-aspect-7-14-15.sh
+++ ModSpherocyl/jobs/sphero-aspect-7-14-15.sh
@@ -4,14 +4,17 @@ dirname=SC-AR-Packings-7-14-15
 
 mkdir ~/Packing/ModSpherocyl/Cluster/$dirname
 
+b=$(( ($1-1)*1500 ))
+c=$((b+1500))
+
 for k in $(seq 3); do
 for j in $(seq 9); do
 for i in $(seq 1000); do
     a=$(( (k-1)*10000+(j-1)*1000+(i-1) ))
-    b=$(( ($1 - 1)*1500 ))
+    b=$(( ($1-1)*1500 ))
     c=$((b+1500))
-    if [ "$a" -ge "$b" ]; then
-    if [ "$a" -lt "$c" ]; then
+    if ((a >= b)); then
+    if ((a < c)); then
     echo cd ~/Packing/ModSpherocyl/Cluster/$dirname '&&' ../ModSpherocyl.out --seed $((1435762516+a)) --filename $((1435762516+a)).txt --ar $k.$j
     fi
     fi
@@ -19,10 +22,10 @@ done
 done
 for i in $(seq 1000); do
     a=$(( k*10000+(i-1) ))
-    b=$(( ($1 - 1)*1500 ))
+    b=$(( ($1-1)*1500 ))
     c=$((b+1500))
-    if [ "$a" -ge "$b" ]; then
-    if [ "$a" -lt "$c" ]; then
+    if ((a >= b)); then
+    if ((a < c)); then
     echo cd ~/Packing/ModSpherocyl/Cluster/$dirname '&&' ../ModSpherocyl.out --seed $((1435762516+a)) --filename $((1435762516+a)).txt --ar $((k+1)).0
     fi
     fi
