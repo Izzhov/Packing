0cdd29f443b7d802870bfcd932aa328399ea5610
Kyle VanderWerf <kylevanderwerf@yahoo.com>
2015-07-14 16:54:16 -0400

    Updated shell script to make it easier to make 20 tasks files

--- ModSpherocyl/jobs/sphero-aspect-7-14-15.sh
+++ ModSpherocyl/jobs/sphero-aspect-7-14-15.sh
@@ -7,10 +7,24 @@ mkdir ~/Packing/ModSpherocyl/Cluster/$dirname
 for k in $(seq 3); do
 for j in $(seq 9); do
 for i in $(seq 1000); do
-    echo cd ~/Packing/ModSpherocyl/Cluster/$dirname '&&' ../ModSpherocyl.out --seed $((1435762516+(k-1)*10000+(j-1)*1000+(i-1))) --filename $((1435762516+(k-1)*10000+(j-1)*1000+(i-1))).txt --ar $k.$j
+    a=$(( (k-1)*10000+(j-1)*1000+(i-1) ))
+    b=$(( ("$1"-1)*1500 ))
+    c=$((b+1500))
+    if [ "$a" -ge "$b" ]
+    if [ "$a" -lt "$c" ]
+    echo cd ~/Packing/ModSpherocyl/Cluster/$dirname '&&' ../ModSpherocyl.out --seed $((1435762516+a)) --filename $((1435762516+a)).txt --ar $k.$j
+    fi
+    fi
 done
 done
 for i in $(seq 1000); do
-    echo cd ~/Packing/ModSpherocyl/Cluster/$dirname '&&' ../ModSpherocyl.out --seed $((1435762516+k*10000+(i-1))) --filename $((1435762516+(k-1)*10000+(j-1)*1000+(i-1))).txt --ar $((k+1)).0
+    a=$(( k*10000+(i-1) ))
+    b=$(( ("$1"-1)*1500 ))
+    c=$((b+1500))
+    if [ "$a" -ge "$b" ]
+    if [ "$a" -lt "$c" ]
+    echo cd ~/Packing/ModSpherocyl/Cluster/$dirname '&&' ../ModSpherocyl.out --seed $((1435762516+a)) --filename $((1435762516+a)).txt --ar $((k+1)).0
+    fi
+    fi
 done
 done
