719ea61c802a791d125fb198d892b16572c2a273
Kyle VanderWerf <kylevanderwerf@yahoo.com>
2015-07-14 16:57:42 -0400

    fixed improved aspect ratio packings shell script

--- ModSpherocyl/jobs/sphero-aspect-7-14-15.sh
+++ ModSpherocyl/jobs/sphero-aspect-7-14-15.sh
@@ -10,8 +10,8 @@ for i in $(seq 1000); do
     a=$(( (k-1)*10000+(j-1)*1000+(i-1) ))
     b=$(( ("$1"-1)*1500 ))
     c=$((b+1500))
-    if [ "$a" -ge "$b" ]
-    if [ "$a" -lt "$c" ]
+    if [ "$a" -ge "$b" ]; then
+    if [ "$a" -lt "$c" ]; then
     echo cd ~/Packing/ModSpherocyl/Cluster/$dirname '&&' ../ModSpherocyl.out --seed $((1435762516+a)) --filename $((1435762516+a)).txt --ar $k.$j
     fi
     fi
@@ -21,8 +21,8 @@ for i in $(seq 1000); do
     a=$(( k*10000+(i-1) ))
     b=$(( ("$1"-1)*1500 ))
     c=$((b+1500))
-    if [ "$a" -ge "$b" ]
-    if [ "$a" -lt "$c" ]
+    if [ "$a" -ge "$b" ]; then
+    if [ "$a" -lt "$c" ]; then
     echo cd ~/Packing/ModSpherocyl/Cluster/$dirname '&&' ../ModSpherocyl.out --seed $((1435762516+a)) --filename $((1435762516+a)).txt --ar $((k+1)).0
     fi
     fi
